b06b66f0c961ea2ac39317dc012764e6e0af02fd
otkuda <kirkot.0306@gmail.com>
2023-03-16 19:13:18 +0300

    resolvons troubles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function getRandomInt(min, max) {
   return Math.floor(min + Math.random() * (max - min));
 }
-/*
+
 function checkCommentLength(string, requiredLength) {
   if(string.length <= requiredLength) {
     return true;
@@ -9,28 +9,10 @@ function checkCommentLength(string, requiredLength) {
     return false;
   }
 }
-*/
+
 
 const button = document.querySelector('.img-upload__label');
 button.onclick = function() {
-  generateContent(25);
+  getRandomInt(1, 100);
+  checkCommentLength('sdgs', 3);
 };
-
-function generateContent(size) {
-  const content = [];
-  for(let i = 0; i < size - 1; i++) {
-    content[i] = generateObj(i + 1);
-  }
-  return content;
-}
-
-function generateObj(id) {
-  const newObj = {
-    id: id,
-    url: `photos/${id}.jpg`,
-    description: '',
-    likes: getRandomInt(15, 200),
-    comments: getRandomInt(0, 200)
-  };
-  return newObj;
-}
